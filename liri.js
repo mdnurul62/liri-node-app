33c6806337bd435fd58f9b40909ded416619e553
mdnurul62 <nurul.alam1@hotmail.com>
2017-06-12 22:25:31 -0400

    adds

--- liri.js
+++ liri.js
@@ -14,25 +14,6 @@ var userSearch = process.argv[3];
 
 //User commands: 'my-tweets', 'soptify-this-song', 'movie-this', 'do-what-it-says'.
 //User searches:'<song name>', '<movie name>'
-//switch-case statement for user command
-
-switch (userCom) {
-	case 'my-tweets':
-		fetchTwitter();
-		break;
-
-	case 'soptify-this-song':
-		fetchSpotify();
-		break;
-
-	case 'movie-this':
-		fetchMovie();
-		break;
-
-	case 'do-what-it-says':
-		fetchRandom();
-		break;
-};
 
 //Call twitter functions
 
@@ -61,39 +42,37 @@ function fetchTwitter() {
 
 //Call spotify function
 function fetchSpotify() {
+	var spotify = require('spotify');
 	var searchObj = {
 			type: 'track',
-			query: "songName"
+			query: "whatToFind"
 		}
+ 
+	spotify.search(searchObj, function(err, data) {
+		if (userSearch != undefined) {
+    		if ( err ) {
+        		console.log('Error occurred: ' + err);
+        		return;
+    		}
+ 
+    	// Do something with 'data'
+    	var trackCount = data.tracks.items.length;
+    	console.log('Found: ' + trackCount + ' Tracks for query: ' + whatToFind + '\n');
 
-	if (userSearch === 'undefined') {
-		var fileName = require('./random.txt')
-		fs.readFile(fileName, 'utf8', function(err, fileContents) {
-			if (err) {
-				return console.log(err);
-			} else {
-			console.log(fileContents.split(','));
-			} 
-
-		});
-
-	} else {
-			spotify.search(searchObj, callback);
+    	 for (var i = 0; i < trackCount; i++) {
+                        var artistCount = data.tracks.items[i].artists.length;
+                        for (var j = 0; j < artistCount; j++) {
+                            console.log("Artist: " + data.tracks.items[i].artists[j].name);
+                            console.log(" Album:  " + wordwrap.wrap(data.tracks.items[i].album.name, { width: 60 }));
+                            console.log(" Link: " + data.tracks.items[i].artists[j].external_urls.spotify + "\n");
+                        }
+         		}
 		}
-
-		function callback(err, data) {
-			if (err) {
-				console.log('Error occurred: ' + err);
-				return;
-			} else {
-				console.log('Tracks: ' + data.tracks[0]);
-				console.log('Artist: ' + data.tracks[0].artist[0]);
-				console.log('Album: ' + data.tracks[0].album);
-				console.log('Spottify link: ' + data.tracks[0].external_url.spotify);
-			}
-		};
-	
+	})
+	fetchSpotify();
 };
+
+	
 //Call movie function
 
 function fetchMovie(){
@@ -102,8 +81,8 @@ function fetchMovie(){
 		var movieName = "Mr. Nobody";
 		console.log(movieName);
 	} else {
-		var movieName = '';
-		var requestURL = "http://www.omdbapi.com/?t=" + movieName + "&tomatoes=true&y=&plot=short&r=json";
+		//var movieName = '';
+		var requestURL = "http://www.omdbapi.com/?t=" + userSearch + "&tomatoes=true&y=&plot=short&r=json";
 		request(requestURL, callback);
 	}
 
@@ -112,33 +91,71 @@ function fetchMovie(){
 			console.log('Error occurred' + err)
 		} else {
 			console.log("---------------------------------------------");
-			console.log("The movie's title is: " + JSON.parse(data)["Title"]);
-			console.log("The movie's release year is: " + JSON.parse(data)["Year"]);		
-			console.log("The movie's rating is: " + JSON.parse(data)["imdbRating"]);
-			console.log("The movie's was produced in: " + JSON.parse(data)["Country"]);
-			console.log("The movie's language is: " + JSON.parse(data)["Language"]);
-			console.log("The movie's plot: " + JSON.parse(data)["Plot"]);
-			console.log("The movie's actors: " + JSON.parse(data)["Actors"]);
-			console.log("The movie's Rotten Tomatoes Rating: " + JSON.parse(data)["tomatoRating"]);
-			console.log("The movie's Rotten Tomatoes URL: " + JSON.parse(data)["tomatoURL"]);
+			console.log("\nMy Movie: ");
+	   		console.log("\nTitle: " + JSON.stringify(data).Title);
+	   		console.log("Year: " + JSON.stringify(data).Year);
+	   		console.log("Rated: " + JSON.stringify(data).Rated);
+	   		console.log("Country: " + JSON.stringify(data).Country);
+	   		console.log("Language: " + JSON.stringify(data).Language);
+	   		console.log("Plot: " + JSON.stringify(data).Plot);
+	   		console.log("Actors: " + JSON.stringify(data).Actors);
+	   		console.log("Rotten Tomatoes: " + JSON.stringify(data).Value);
+	   		console.log("URL: https://www.rottentomatoes.com/search/?search=" + userSearch);
 			console.log("-----------------------------------------------");
+		
 		}
-	};
+	}
 };
+
 //Call random function
 function fetchRandom(){
 	var fileName = require('./random.txt');
+	var writeThis = [process.argv[3], process.argv[4]];
+
+	fs.writeFile('random.txt', writeThis, function(error){
+		if (error) {
+			console.log('error');
+		} else {
+			console.log('created the file');
+		}
+	});
+
+
 	fs.readFile(fileName, 'utf8', function(err, fileContents){
 		if (err) {
 			return console.log(err)
 		} else {
 			console.log(fileContents);
-			//var dataArr = data.split(',');
-			//var randomUserCom = dataArr[0];
-			//var randomUserSearch = dataArr[1];
+			var array = data.split(',');
 
-			//console.log("You requested to " + "<" + randomUserCom + "> with " + randomUserSearch);
-			//appendFile("You requested to " + "<" + randomUserCom + "> with " + randomUserSearch);
+			userCom = array[0];
+			userSearch = array [1];
+
+			switchCase();
 		}
 	});
 };
+
+
+//switch-case statement for user command
+function switchCase() {
+	switch (userCom) {
+		case 'my-tweets':
+			fetchTwitter();
+			break;
+
+		case 'soptify-this-song':
+			fetchSpotify();
+			break;
+
+		case 'movie-this':
+			fetchMovie();
+			break;
+
+		case 'do-what-it-says':
+			fetchRandom();
+			break;
+	}
+};
+
+switchCase();
